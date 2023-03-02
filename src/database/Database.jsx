efeea73f04f4e92d67eef2f1889ea4bd17367210
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-03 02:26:05 +0400

    updated urls for github and live versions

--- src/database/Database.jsx
+++ src/database/Database.jsx
@@ -2,38 +2,37 @@ import BlogDP from "../assets/projects/blog.png";
 import CalcDP from "../assets/projects/calculator.png";
 import RockPaperDP from "../assets/projects/rockPaper.png";
 import StarMatchDP from "../assets/projects/starMatch.png";
-import WorkImg from "../assets/projects/workImg.jpeg";
 
 export const database = [
   {
     id: 1,
     name: "HTML CSS Blog",
     image: BlogDP,
-    github: "http://www.google.com",
-    live: "",
+    github: "https://github.com/HamzaSheikh05/Main_Assignment_Two",
+    live: "https://hs-basic-blog.netlify.app/",
   },
 
   {
     id: 2,
     name: "JavaScript Calculator",
     image: CalcDP,
-    github: "",
-    live: "",
+    github: "https://github.com/HamzaSheikh05/JS-Calculator",
+    live: "https://hamza-calculator.netlify.app/",
   },
 
   {
     id: 3,
     name: "Rock Paper Scissors Game",
     image: RockPaperDP,
-    github: "",
-    live: "",
+    github: "https://github.com/HamzaSheikh05/rock-scissor-paper-ui",
+    live: "https://hamza-rockpaper-game.netlify.app/",
   },
 
   {
     id: 4,
     name: "Star Match Game",
     image: StarMatchDP,
-    github: "",
-    live: "",
+    github: "https://github.com/HamzaSheikh05/Star-Match-Game",
+    live: "https://hs-star-match-game.netlify.app/",
   },
 ];
