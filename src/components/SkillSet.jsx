9ffab8cad26989e415823e32db77077da0cceee5
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-02 23:22:39 +0400

    added images and created hard coded database

--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -15,7 +15,7 @@ export const SkillSet = () => {
         <p className="text-4xl text-blue-300 font-bold inline border-b-2 border-blue-400 font-burtons">
           Technical Skills
         </p>
-        <p className="py-4">The Tech I am experienced in</p>
+        <p className="py-4 text-md font-serif">The Tech I am experienced in</p>
       </div>
 
       <div className="flex justify-evenly items-center text-center p-10 max-lg:flex max-lg:flex-wrap">
@@ -23,30 +23,37 @@ export const SkillSet = () => {
           <img className="w-24 mx-auto" src={HTML} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">HTML</p>
         </div>
+
         <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
           <img className="w-24 mx-auto" src={CSS} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">CSS</p>
         </div>
+
         <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
           <img className="w-24 mx-auto" src={JavaScript} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">JavaScript</p>
         </div>
+
         <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
           <img className="w-24 mx-auto" src={ReactImg} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">React</p>
         </div>
+
         <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
           <img className="w-24 mx-auto" src={GitHub} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">Github</p>
         </div>
+
         <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
           <img className="w-24 mx-auto" src={Node} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">Node</p>
         </div>
+
         <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
           <img className="w-24 mx-auto" src={Tailwind} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">Tailwind</p>
         </div>
+
         <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
           <img className="w-24 mx-auto" src={FireBase} alt="HTML icon" />
           <p className="mt-2 font-serif font-semibold">FireBase</p>
---
+++ src/database/Database.jsx
@@ -0,0 +1,38 @@
+import BlogDP from "../assets/projects/blog.png";
+import CalcDP from "../assets/projects/calculator.png";
+import RockPaperDP from "../assets/projects/rockPaper.png";
+import StarMatchDP from "../assets/projects/starMatch.png";
+
+export const database = [
+  {
+    id: 1,
+    name: "HTML CSS Blog",
+    image: BlogDP,
+    github: "",
+    live: "",
+  },
+
+  {
+    id: 2,
+    name: "JavaScript Calculator",
+    image: CalcDP,
+    github: "",
+    live: "",
+  },
+
+  {
+    id: 3,
+    name: "Rock Paper Scissors Game",
+    image: RockPaperDP,
+    github: "",
+    live: "",
+  },
+
+  {
+    id: 4,
+    name: "Star Match Game",
+    image: StarMatchDP,
+    github: "",
+    live: "",
+  },
+];
