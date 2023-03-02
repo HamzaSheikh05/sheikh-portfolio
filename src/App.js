95486afc456780c0af7fda524eefffc0789df2b6
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-02 22:49:41 +0400

    completed skillset Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { CardList } from "./components/CardList";
 import { Header } from "./components/Header";
 import { Intro } from "./components/Intro";
 import { Portfolio } from "./components/Portfolio";
+import { SkillSet } from "./components/SkillSet";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -25,6 +26,10 @@ function App() {
       <main className="bg-inherit px-10 dark:bg-gradient-to-r from-[#000428] via-[#000011] to-[#000428]">
         <section className="mb-3">
           <Intro />
+        </section>
+
+        <section className="bg-inherit px-10 dark:bg-gradient-to-r from-[#000428] via-[#000011] to-[#000428]">
+          <SkillSet />
         </section>
 
         <section className="mb-3">
--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -1,5 +1,58 @@
 import React from "react";
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node.png";
+import FireBase from "../assets/firebase.png";
+import GitHub from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
+import Mongo from "../assets/mongo.png";
 
 export const SkillSet = () => {
-  return <div>SkillSet</div>;
+  return (
+    <div className="mx-auto py-4 flex flex-col justify-between text-gray-300">
+      <div>
+        <p className="text-4xl text-blue-300 font-bold inline border-b-2 border-blue-400 font-burtons">
+          Technical Skills
+        </p>
+        <p className="py-4">The Tech I am experienced in</p>
+      </div>
+
+      <div className="flex justify-evenly items-center text-center p-10">
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={HTML} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">HTML</p>
+        </div>
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={CSS} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">CSS</p>
+        </div>
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={JavaScript} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">JavaScript</p>
+        </div>
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={ReactImg} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">React</p>
+        </div>
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={GitHub} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">Github</p>
+        </div>
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={Node} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">Node</p>
+        </div>
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={Tailwind} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">Tailwind</p>
+        </div>
+        <div className="bg-transparent shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-6 rounded-md items-center">
+          <img className="w-24 mx-auto" src={FireBase} alt="HTML icon" />
+          <p className="mt-2 font-serif font-semibold">FireBase</p>
+        </div>
+      </div>
+    </div>
+  );
 };
